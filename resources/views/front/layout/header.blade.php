6f1330a69a7af657379f676d9312c53562353779
Sulaiman Barki <99899527+sulaimanbarki@users.noreply.github.com>
2025-05-23 18:12:12 +0500

    Update header.blade.php

--- resources/views/front/layout/header.blade.php
+++ resources/views/front/layout/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <a class="navbar-brand logo" href="{{ route('front.home') }}">
-            <img src="{{ $logo_path->logo_path }}" alt="" /></a>
+            <img src="{{ $logo_path->logo_path }}" alt="" style="height: 50px; width: auto;"  /></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
